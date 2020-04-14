e425dbde4b593666eef976f38aa45d1343e4aac9
Sibusiso <itsbudamain.05@gmail.com>
2020-04-14 13:38:20 +0200

    giving user the option to select a platform

--- autopaper.py
+++ autopaper.py
@@ -1,12 +1,22 @@
 import request
 
 #Unsplash API 
-#Pixabay API
-API_URL = '' 
+#Pixabay API 
 #Pexels API
 
+options = ['unsplash','pexels','pixabay']
+
 def main():
-	response_obj = request.get()
+	platform = input("Which platform would you want to you would like to find images on(unsplash, pexels, or pixabay): ");
+	platform.lower();
+
+	#foricing the user to choose an option
+	while not(platform in options):
+		platform = input('Please choose between unsplash, pexels and pixabay: ');
+		platform.lower()
+
+	print('\n Thank you :)')
+	# response_obj = request.get()
 
 if __name__ == '__main__':
 	main()
